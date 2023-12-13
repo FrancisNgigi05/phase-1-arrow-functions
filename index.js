c69b6bf7ff3b07998cbbf4e6d87fc9a4c5b0f482
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2023-12-13 07:36:10 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function() {
+    let result = 2000 / 100;
+    return (result);
+}
+
+const square = (num3) => {
+    return (num3 ** 2);
+}
+
+let squareResult = square(3);
+
+const add  = (num4, num5) => {
+    return (num4 + num5);
+}
+
